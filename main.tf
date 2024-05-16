b5f1bbce1d35d350bb64589d01c8c150aa05ffe0
souhaomrani2 <163522102+souhaomrani2@users.noreply.github.com>
2024-05-16 23:36:21 +0200

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    proxmox = {
+      source  = "telmate/proxmox"
+      version = "3.0.1-rc1"
+    }
+  }
+}
+
+provider "proxmox" {
+  pm_api_url         = var.pm_api_url
+  pm_api_token_id    = var.pm_user
+  pm_api_token_secret = var.pm_password
+  pm_tls_insecure    = true
+}
+
+resource "proxmox_vm_qemu" "ubuntu_vmm" {
+  name        = "ubuntu-vmm"
+  clone       = var.template
+  target_node = "pve"
+}
+
+  
